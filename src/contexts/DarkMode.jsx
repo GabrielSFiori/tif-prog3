d67e185085d009af3dba0fe39e9b6018dc11a8ea
Gabriel <g.fioripas@gmail.com>
2024-08-03 13:30:59 -0300

    Changes to be committed: modified:   src/components/Articles/styles/ArticleDetail.css modified:   src/components/NavBar.jsx modified:   src/contexts/DarkMode.jsx modified:   src/routes/styles/HomeScreen.css modified:   src/styles.css

--- src/contexts/DarkMode.jsx
+++ src/contexts/DarkMode.jsx
@@ -5,7 +5,7 @@ export const DarkMode = () => {
   const { theme, toggleTheme } = useContext(DarkModeContext);
 
   return (
-    <button className="button is-primary ml-2" onClick={toggleTheme}>
+    <button className="button is-primary mb-3 mr-3" onClick={toggleTheme}>
       <span className="icon">
         <i className={theme === "dark" ? "fas fa-sun" : "fas fa-moon"}></i>
       </span>
