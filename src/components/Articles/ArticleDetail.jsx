d834bab5ca9528ad16eca01e977d00c3d983841d
Gabriel <g.fioripas@gmail.com>
2024-08-11 11:58:44 -0300

    modified:   src/components/Articles/ArticleDetail.jsx

--- src/components/Articles/ArticleDetail.jsx
+++ src/components/Articles/ArticleDetail.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect, useContext } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import {
   fetchArticleById,
   fetchCategories,
   fetchDeleteArticle,
 } from "../../hooks/ConnApi";
-import { fetchCountReactions, postReaction } from "../../hooks/ConReactions";
-import Reactions from "../Reaction/Reaction"; // Asegúrate de usar `default export` en Reaction.jsx
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 import "./styles/ArticleDetail.css";
 import ViewComments from "../Comments/ViewComments";
@@ -15,22 +14,28 @@ import DeleteComments from "../Comments/DeleteComments";
 export const ArticleDetail = () => {
   const { id } = useParams();
   const [article, setArticle] = useState(null);
+  const [categories, setCategories] = useState([]);
   const [categoriesMap, setCategoriesMap] = useState({});
-  const [reactionCount, setReactionCount] = useState(0);
   const [error, setError] = useState(null);
   const { auth } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  const handleReactionClick = async (reactionType) => {
-    try {
-      const response = await postReaction(article.id, reactionType, auth.token);
-      const updatedArticle = { ...article, reactions: response.reactions };
-      setArticle(updatedArticle);
-      const { count } = await fetchCountReactions();
-      setReactionCount(count);
-    } catch (error) {
-      console.error("Error adding reaction:", error);
-      setError(error.message);
+  const handleEdit = () => {
+    navigate(`/article/edit-article/${article.id}`);
+  };
+
+  const handleDelete = async () => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this article?"
+    );
+    if (confirmDelete) {
+      try {
+        await fetchDeleteArticle(article.id, auth.token);
+        navigate("/articles");
+      } catch (error) {
+        console.error("Error deleting article:", error);
+        setError(error.message);
+      }
     }
   };
 
@@ -43,14 +48,14 @@ export const ArticleDetail = () => {
       try {
         const articleData = await fetchArticleById(id);
         setArticle(articleData);
+        setCategories(articleData.categories);
+
         const categoriesData = await fetchCategories();
         const categoriesMap = categoriesData.reduce((map, category) => {
           map[category.id] = category.name;
           return map;
         }, {});
         setCategoriesMap(categoriesMap);
-        const { count } = await fetchCountReactions();
-        setReactionCount(count);
       } catch (error) {
         setError(error.message);
       }
@@ -95,6 +100,7 @@ export const ArticleDetail = () => {
           style={{
             display: "-webkit-box",
             WebkitBoxOrient: "vertical",
+
             overflow: "hidden",
             textOverflow: "ellipsis",
           }}
@@ -117,15 +123,11 @@ export const ArticleDetail = () => {
           <p className="card-footer-item">
             Tags: {article.tags ? article.tags.join(", ") : "N/A"}
           </p>
+          <p className="card-footer-item">
+            Reactions:{" "}
+            {article.reactions ? article.reactions.join(", ") : "N/A"}
+          </p>
         </footer>
-
-        <div className="has-text-centered mt-4">
-          <p>Total Reactions: {reactionCount}</p>
-        </div>
-
-        <div className="reactions-section">
-          <Reactions onClick={handleReactionClick} />
-        </div>
 
         <ViewComments articleId={article.id} />
 
@@ -135,29 +137,10 @@ export const ArticleDetail = () => {
           </button>
           {auth.isAuthenticated && (
             <>
-              <a
-                className="button is-info ml-4"
-                onClick={() => navigate(`/article/edit-article/${article.id}`)}
-              >
+              <a className="button is-info ml-4" onClick={handleEdit}>
                 Edit
               </a>
-              <a
-                className="button is-danger ml-4"
-                onClick={async () => {
-                  const confirmDelete = window.confirm(
-                    "Are you sure you want to delete this article?"
-                  );
-                  if (confirmDelete) {
-                    try {
-                      await fetchDeleteArticle(article.id, auth.token);
-                      navigate("/articles");
-                    } catch (error) {
-                      console.error("Error deleting article:", error);
-                      setError(error.message);
-                    }
-                  }
-                }}
-              >
+              <a className="button is-danger ml-4" onClick={handleDelete}>
                 Delete
               </a>
             </>
@@ -167,3 +150,15 @@ export const ArticleDetail = () => {
     </div>
   );
 };
+
+import React, { useState, useEffect, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import {
+  fetchArticleById,
+  fetchCategories,
+  fetchDeleteArticle,
+} from "../../hooks/ConnApi";
+import { fetchCountReactions, postReaction } from "../../hooks/ConReactions";
+import Reactions from "../Reaction/Reaction"; // Asegúrate de usar `default export` en Reaction.jsx
+import { AuthContext } from "../../contexts/AuthContext";
+import "./styles/ArticleDetail.css";
